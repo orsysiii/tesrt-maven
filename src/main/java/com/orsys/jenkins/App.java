e7b74ced10d20ec35b422db1256e662b44a1a2f1
hakim <hdb.hakim@gmail.com>
2024-10-18 11:09:37 +0100

    first commit

--- src/main/java/com/orsys/jenkins/App.java
+++ src/main/java/com/orsys/jenkins/App.java
@@ -6,6 +6,6 @@ package com.orsys.jenkins;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World again!");
+        
     }
 }
